681fecb90ad42b44847fa77898a3902777b54cbe
Joe Bigio <jvbigio@gmail.com>
2021-09-01 20:43:50 -0400

    Added app.get /* to server.js

--- server.js
+++ server.js
@@ -8,4 +8,8 @@ const port = process.env.PORT || 3000
 
 app.use('/', express.static(path.join(__dirname, 'client/build')))
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
+
 app.listen(port, () => console.log(`Server running on port: ${port}`))
