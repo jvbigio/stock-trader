65c1d52608073e03b9342f0d7a4173a9e3152654
Joe Bigio <jvbigio@gmail.com>
2021-09-21 02:06:26 -0400

    cleaned up comments

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,34 +5,13 @@ import { NavLink } from 'react-router-dom'
 import { css } from '@emotion/react'
 
 import { AppBar, Toolbar, Typography, IconButton, Button, Drawer, Tab, Tabs, MenuIcon } from '@mui/material'
-// import { MenuIcon } from '@mui/material'
-// import useStyles from '../styles'
-// import { flexbox } from '@material-ui/system'
 
 import { GoHome, GoBriefcase, GoRepo } from 'react-icons/go'
 
-// import { makeStyles } from '@material-ui/core/styles'
-// import { MenuIcon } from '@material-ui/icons/Menu';
 // This branch updated dependencies for v5 material ui
-
-// NAVBAR-TEST BRANCH - VID FROM https://youtu.be/mAPT9Xh6NKE
-
-// const useStyles = makeStyles((theme) => ({
-//   navbar: {
-//     backgroundColor: '#368727'
-//   },
-//   pages: {
-//     flexGrow: 1
-//   },
-//   icon: {
-
-//   }
-// }))
 
 const Navbar = () => {
   const [value, setValue] = useState(0)
-
-  // const classes = useStyles()
 
   const handleClickTab = (e, newValue) => {
     setValue(newValue)
