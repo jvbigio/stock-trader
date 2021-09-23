29997c140acc2ea09c4f54a9d7cba5dc40b6e030
Joe Bigio <jvbigio@gmail.com>
2021-09-23 09:58:58 -0400

    Added router to drawer component. working so far

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Home from './pages/Home'
 import Portfolio from './pages/Portfolio'
 import Report from './pages/Report'
 import Login from './pages/Login'
+
 import './App.css'
 
 function App () {
@@ -19,7 +20,7 @@ function App () {
         <main className='card'>
           <Switch>
             <Route path='/' exact component={Home} />
-            <Route path='/summary' component={Portfolio} />
+            <Route path='/portfolio' component={Portfolio} />
             <Route path='/report' component={Report} />
             <Route path='/login' component={Login} />
           </Switch>
--- client/src/components/DrawerComponent.js
+++ client/src/components/DrawerComponent.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 
 import { Drawer, List, ListItem, ListItemText, ListItemIcon, ListItemButton, IconButton } from '@mui/material'
 
@@ -7,15 +8,60 @@ import BusinessCenterIcon from '@mui/icons-material/BusinessCenter'
 import AssessmentIcon from '@mui/icons-material/Assessment'
 import LoginIcon from '@mui/icons-material/Login'
 import MenuIcon from '@mui/icons-material/Menu'
-// import { margin } from '@mui/system'
-
-import './DrawerComponent.css'
 
 const DrawerComponent = () => {
   const [openDrawer, setOpenDrawer] = useState(true)
 
   return (
     <>
+      <Drawer
+        anchor='left'
+        onClose={() => setOpenDrawer(false)}
+        open={openDrawer}
+      >
+        <List>
+          <ListItem divider button component={NavLink} to='/'>
+            <ListItemIcon>
+              <HomeIcon sx={{ mr: 1 }} />
+              <ListItemText primary='Home' />
+            </ListItemIcon>
+          </ListItem>
+          <ListItem divider button component={NavLink} to='/portfolio'>
+            <ListItemIcon>
+              <BusinessCenterIcon sx={{ mr: 1 }} />
+              <ListItemText primary='Portfolio' />
+            </ListItemIcon>
+          </ListItem>
+          <ListItem divider button component={NavLink} to='/report'>
+            <ListItemIcon>
+              <AssessmentIcon sx={{ mr: 1 }} />
+              <ListItemText primary='Report' />
+            </ListItemIcon>
+          </ListItem>
+          <ListItem button component={NavLink} to='/login'>
+            <ListItemIcon>
+              <LoginIcon sx={{ mr: 1 }} />
+              <ListItemText primary='Login' />
+            </ListItemIcon>
+          </ListItem>
+        </List>
+      </Drawer>
+      <IconButton
+        sx={{ ml: 'auto' }}
+        color='inherit'
+        aria-label='open drawer'
+        anchor='right'
+        onClick={() => setOpenDrawer(!openDrawer)}
+      >
+        <MenuIcon />
+      </IconButton>
+    </>
+  )
+}
+
+// orig
+/*
+<>
       <Drawer
         anchor='left'
         onClose={() => setOpenDrawer(false)}
@@ -58,7 +104,6 @@ const DrawerComponent = () => {
         <MenuIcon />
       </IconButton>
     </>
-  )
-}
+*/
 
 export default DrawerComponent
