e6446d4c1c1288c3d914ce2e79ecffe1d20a7f3e
Joe Bigio <jvbigio@gmail.com>
2021-09-29 11:11:29 -0400

    Used material for homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import './Home.css'
 
-// import { Image } from 'react-bootstrap/'
+import { Box, Typography, Paper } from '@mui/material'
 
 import chart from '../images/data-chart.jpg'
 
 const Home = () => {
   return (
-    <div className='flex-wrapper'>
-      <header className='intro'>
+    <Box
+      sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', width: '100%' }}
+    >
+      <Typography variant='h4' component='div' paragraph gutterBottom mt={2}>
         Simple. Intuitive. Fast.
         <br />
         Test drive StoX's fantasy trading experience
-      </header>
-      <main className='content-wrapper' />
-    </div>
+      </Typography>
+    </Box>
   )
 }
+
+// orig
+// const Home = () => {
+//   return (
+//     <div className='flex-wrapper'>
+//       <header className='intro'>
+//         Simple. Intuitive. Fast.
+//         <br />
+//         Test drive StoX's fantasy trading experience
+//       </header>
+//       <main className='content-wrapper'>
+//         <div className='image-bg' />
+//       </main>
+//     </div>
+//   )
+// }
 
 export default Home
