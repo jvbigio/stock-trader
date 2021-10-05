694fa976e9f1c0fde9c94947f96a564cc201eeb9
Joe Bigio <jvbigio@gmail.com>
2021-10-05 11:35:49 -0400

    deleted comments

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,10 +10,6 @@ import analysis from '../images/analysis.jpg'
 const theme = createTheme()
 
 theme.typography.h4 = {
-  // fontSize: '1.2rem',
-  // '@media (min-width: 600px': {
-  //   fontSize: '1.5rem'
-  // },
   [theme.breakpoints.up('sm')]: {
     fontSize: '2rem'
   }
