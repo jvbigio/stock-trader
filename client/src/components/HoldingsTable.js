420d497f299aaa9958ab0225716fa6424797bc5c
Joe Bigio <jvbigio@gmail.com>
2021-10-06 15:36:46 -0400

    Added 6 holdings. Working on pagination

--- client/src/components/HoldingsTable.js
+++ client/src/components/HoldingsTable.js
@@ -1,21 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Box, Link, TableContainer, Table, TableBody, TableCell, TableHead, TableRow, Title, Paper, Typography } from '@mui/material'
+import { Box, Link, TableContainer, Table, TableBody, TableCell, TableHead, TableRow, TablePagination, Title, Paper, Typography } from '@mui/material'
 
 function createData (id, name, symbol, price, currentValue, quantity) {
   return { id, name, symbol, price, currentValue, quantity }
 }
 
-const ROWS = [
+const rows = [
   createData(0, 'AMC Entertainment Holdings Inc', 'AMC', 36.01, 1800.50, 50),
   createData(1, 'Alphabet Inc', 'GOOGL', 2727.13, 54542.60, 20),
   createData(2, 'Novavax Inc', 'NVAX', 164.21, 15536.80, 80),
   createData(3, 'Facebook Inc', 'FB', 331.45, 16572.50, 50),
   createData(4, 'Apple Inc', 'AAPL', 139.79, 6989.50, 50),
-  createData(5, 'GameStop Corp', 'GME', 168.26, 5047.80, 30)
+  createData(5, 'GameStop Corp', 'GME', 168.26, 5047.80, 30),
+  createData(6, 'Shopify Inc', 'SHOP', 1348.53, 1348.53, 10)
 ]
 
 export const HoldingsTable = () => {
+  const [selected, setSelected] = useState([])
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(5)
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name)
+    let newSelected = []
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name)
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1))
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1))
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      )
+    }
+
+    setSelected(newSelected)
+  }
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10))
+    setPage(0)
+  }
+
+  const isSelected = (name) => selected.indexOf(name) !== -1
+
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0
+
   return (
     <>
       {/* <TableContainer component={Paper}> */}
@@ -31,7 +69,7 @@ export const HoldingsTable = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {ROWS.map((row) => (
+          {rows.map((row) => (
             <TableRow key={row.id}>
               <TableCell>{row.name}</TableCell>
               <TableCell align='right'>{row.symbol}</TableCell>
@@ -42,6 +80,15 @@ export const HoldingsTable = () => {
           ))}
         </TableBody>
       </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component='div'
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
       {/* </TableContainer> */}
     </>
   )
--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -35,26 +35,5 @@ const Portfolio = () => {
   // </ThemeProvider>
   )
 }
-// const Summary = () => {
-//   return (
-//     // <div className='flex-wrapper'>
-//     <div className='flex-wrapper' style={{ width: '100%', height: '100vh' }}>
-//       <Box
-//         sx={{
-//           display: 'flex',
-//           alignItems: 'flex-start',
-//           p: 1,
-//           m: 1,
-//           bgcolor: 'background.paper',
-//           height: 100
-//         }}
-//       >
-//         <Box sx={{ p: 1, bgcolor: 'grey.300' }}>Item 1</Box>
-//         <Box sx={{ p: 1, bgcolor: 'grey.300', alignSelf: 'flex-end' }}>Item 2</Box>
-//         <Box sx={{ p: 1, bgcolor: 'grey.300' }}>Item 3</Box>
-//       </Box>
-//     </div>
-//   )
-// }
 
 export default Portfolio
