c805979832eec8cf580248cb792e9f550fa4f2b6
Joe Bigio <jvbigio@gmail.com>
2021-10-06 12:40:13 -0400

    Added dummy data in portfolio.js

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,30 +1,48 @@
 import React from 'react'
 import './Portfolio.css'
 
-import Box from '@mui/material/Box'
+import { Box, Link, Table, TableBody, TableCell, TableHead, TableRow, Title } from '@mui/material'
+
+function createData (id, name, symbol, price, currentValue, quantity) {
+  return { id, name, symbol, price, currentValue, quantity }
+}
+
+const ROWS = [
+  createData(0, 'AMC Entertainment Holdings Inc.', 'AMC', 36.01, 1800.50, 50),
+  createData(1, 'Alphabet Inc.', 'GOOGL', 2727.13, 54542.60, 20),
+  createData(2, 'Novavax Inc.', 'NVAX', 164.21, 15536.80, 80),
+  createData(3, 'Facebook Inc.', 'FB', 331.45, 16572.50, 50),
+  createData(4, 'Apple Inc.', 'AAPL', 139.79, 6989.50, 50),
+  createData(5, 'GameStop Corp.', 'GME', 168.26, 5047.80, 30)
+]
 
 const Summary = () => {
   return (
-    // <div className='flex-wrapper'>
-    <div className='flex-wrapper' style={{ width: '100%', height: '100vh' }}>
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'flex-start',
-          p: 1,
-          m: 1,
-          // bgcolor: 'background.paper',
-          bgcolor: 'blue',
-          height: 100,
-          border: '3px solid green'
-        }}
-      >
-        <Box sx={{ p: 1, bgcolor: 'grey.300' }}>Item 1</Box>
-        <Box sx={{ p: 1, bgcolor: 'grey.300', alignSelf: 'flex-end' }}>Item 2</Box>
-        <Box sx={{ p: 1, bgcolor: 'grey.300' }}>Item 3</Box>
-      </Box>
-    </div>
+    <>
+
+    </>
   )
 }
+// const Summary = () => {
+//   return (
+//     // <div className='flex-wrapper'>
+//     <div className='flex-wrapper' style={{ width: '100%', height: '100vh' }}>
+//       <Box
+//         sx={{
+//           display: 'flex',
+//           alignItems: 'flex-start',
+//           p: 1,
+//           m: 1,
+//           bgcolor: 'background.paper',
+//           height: 100
+//         }}
+//       >
+//         <Box sx={{ p: 1, bgcolor: 'grey.300' }}>Item 1</Box>
+//         <Box sx={{ p: 1, bgcolor: 'grey.300', alignSelf: 'flex-end' }}>Item 2</Box>
+//         <Box sx={{ p: 1, bgcolor: 'grey.300' }}>Item 3</Box>
+//       </Box>
+//     </div>
+//   )
+// }
 
 export default Summary
