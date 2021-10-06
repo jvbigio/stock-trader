3fc58f721de4b9b4984d06fdd3f4431da7d7f352
Joe Bigio <jvbigio@gmail.com>
2021-10-06 16:22:36 -0400

    Added data-grid mui via npm

--- client/src/components/HoldingsTable.js
+++ client/src/components/HoldingsTable.js
@@ -1,97 +1,43 @@
 import React, { useState } from 'react'
 
-import { Box, Link, TableContainer, Table, TableBody, TableCell, TableHead, TableRow, TablePagination, Title, Paper, Typography } from '@mui/material'
-
-function createData (id, name, symbol, price, currentValue, quantity) {
-  return { id, name, symbol, price, currentValue, quantity }
-}
+// import { Box, Link, TableContainer, Table, TableBody, TableCell, TableHead, TableRow, TablePagination, Title, Paper, Typography } from '@mui/material'
+// import { DataGridPro } from '@mui/x-data-grid-pro'
+import { DataGrid } from '@mui/x-data-grid'
 
 // THIS BRANCH IS FOR DATA GRID PAGINATION, NOT TABLE PAGINATION
-
-const rows = [
-  createData(0, 'AMC Entertainment Holdings Inc', 'AMC', 36.01, 1800.50, 50),
-  createData(1, 'Alphabet Inc', 'GOOGL', 2727.13, 54542.60, 20),
-  createData(2, 'Novavax Inc', 'NVAX', 164.21, 15536.80, 80),
-  createData(3, 'Facebook Inc', 'FB', 331.45, 16572.50, 50),
-  createData(4, 'Apple Inc', 'AAPL', 139.79, 6989.50, 50),
-  createData(5, 'GameStop Corp', 'GME', 168.26, 5047.80, 30),
-  createData(6, 'Shopify Inc', 'SHOP', 1348.53, 1348.53, 10)
+const columns = [
+  { field: 'id', headerName: 'ID', type: 'number', width: 70 },
+  { field: 'name', headerName: 'Name', width: 130 },
+  { field: 'symbol', headerName: 'Symbol', width: 130 },
+  { field: 'price', headerName: 'Price', type: 'number', width: 90 },
+  { field: 'currentValue', headerName: 'Current Value', type: 'number', width: 90 },
+  { field: 'quantity', headerName: 'Quantity', type: 'number', width: 90 }
 ]
 
-export const HoldingsTable = () => {
-  const [selected, setSelected] = useState([])
+const rows = [
+  { id: 0, name: 'AMC Entertainment Holdings Inc', symbol: 'AMC', price: 36.01, currentValue: 1800.50, quantity: 50 },
+  { id: 1, name: 'Alphabet Inc', symbol: 'GOOGL', price: 2727.13, currentValue: 54542.60, quantity: 20 },
+  { id: 2, name: 'Novavax Inc', symbol: 'NVAX', price: 164.21, currentValue: 15536.80, quantity: 80 },
+  { id: 3, name: 'Facebook Inc', symbol: 'FB', price: 331.45, currentValue: 16572.50, quantity: 50 },
+  { id: 4, name: 'Apple Inc', symbol: 'AAPL', price: 139.79, currentValue: 6989.50, quantity: 50 },
+  { id: 5, name: 'GameStop Corp', symbol: 'GME', price: 168.26, currentValue: 5047.80, quantity: 30 },
+  { id: 6, name: 'Shopify Inc', symbol: 'SHOP', price: 1348.53, currentValue: 1348.53, quantity: 10 }
+]
+
+export default function HoldingsTable () {
   const [page, setPage] = useState(0)
-  const [rowsPerPage, setRowsPerPage] = useState(5)
-
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name)
-    let newSelected = []
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name)
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1))
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1))
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      )
-    }
-
-    setSelected(newSelected)
-  }
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage)
-  }
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10))
-    setPage(0)
-  }
-
-  const isSelected = (name) => selected.indexOf(name) !== -1
-
-  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0
 
   return (
-    <>
-      {/* <TableContainer component={Paper}> */}
-      <Typography variant='h3' textAlign='center' gutterBottom>Holdings</Typography>
-      <Table sx={{ minWidth: 650 }} size='small' aria-label='holdings table'>
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align='right'>Symbol</TableCell>
-            <TableCell align='right'>Price</TableCell>
-            <TableCell align='right'>Current Value</TableCell>
-            <TableCell align='right'>Quantity</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.name}</TableCell>
-              <TableCell align='right'>{row.symbol}</TableCell>
-              <TableCell align='right'>{row.price}</TableCell>
-              <TableCell align='right'>{row.currentValue}</TableCell>
-              <TableCell align='right'>{row.quantity}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 25]}
-        component='div'
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        getRowId={(r) => r.id}
         page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
+        onPageChange={(newPage) => setPage(newPage)}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        pagination
+        checkBoxSelection
       />
-      {/* </TableContainer> */}
-    </>
+    </div>
   )
 }
