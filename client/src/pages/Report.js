3e4e8309177af702d5b139a67e0fa59c544e1469
Joe Bigio <jvbigio@gmail.com>
2021-10-26 04:17:43 -0400

    Added some wrappers for report page charts

--- client/src/pages/Report.js
+++ client/src/pages/Report.js
@@ -1,10 +1,55 @@
 import React from 'react'
 
-const Report = props => {
+import { Container, Box, Paper, Grid, Typography } from '@mui/material'
+
+const Report = () => {
   return (
-    <div>
-      <h1>Report Page</h1>
-    </div>
+    <>
+      <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          {/* Holdings Chart */}
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 240
+              }}
+            >
+              {/* render chart  here */}
+            </Paper>
+          </Grid>
+          {/* Total Value of holdings based on gains/losses */}
+          <Grid item xs={12} md={4} lg={3}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 240
+              }}
+            >
+              {/* Render holdings value here */}
+            </Paper>
+          </Grid>
+          {/* Market Activity */}
+          <Grid item xs={12}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 240
+              }}
+            >
+              {/* Render Market activity chart here */}
+            </Paper>
+          </Grid>
+        </Grid>
+        {/* <Copyright sx={{ pt: 4 }} /> */}
+      </Container>
+    </>
   )
 }
 
