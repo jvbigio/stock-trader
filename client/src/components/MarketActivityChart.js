205f91be8c53167726be7833e5b5b1b6bbf6758b
Joe Bigio <jvbigio@gmail.com>
2021-10-26 18:47:48 -0400

    Finished area dow chart

--- client/src/components/MarketActivityChart.js
+++ client/src/components/MarketActivityChart.js
@@ -7,28 +7,26 @@ const MarketActivityChart = () => {
     <div style={{ display: 'flex', maxWidth: 900 }}>
       <Chart
         // width={400}
-        width={900}
+        // width={900}
+        width={550}
         height='300px'
         chartType='AreaChart'
         loader={<div>Loading Chart</div>}
-        // data={[
-        //   ['Time', 'Index  Average'],
-        //   ['11A', 35861.40],
-        //   ['1P', 35749.52],
-        //   ['3P', 35839.81],
-        //   ['5P', 35756.88]
-        // ]}
         data={[
-          ['Time', 'Index Avg. (in Thousandths)'],
-          ['11A', 35.861],
-          ['1P', 35.749],
-          ['3P', 35.839],
-          ['5P', 35.756]
+          ['Time', 'Index  Average'],
+          ['10A', 35812],
+          ['11A', 35861],
+          ['12P', 35834],
+          ['1P', 35749],
+          ['2P', 35791],
+          ['3P', 35839],
+          ['4P', 35754],
+          ['5P', 35756]
         ]}
         options={{
           title: 'DJIA',
           hAxis: { title: 'TUE', titleTextStyle: { color: '#333' } },
-          vAxis: { minValue: 0 },
+          vAxis: { minValue: 35000 },
           // For the legend to fit, we make the chart area smaller
           // chartArea: { width: '50%', height: '70%' }
           chartArea: { width: '50%', height: '70%' }
