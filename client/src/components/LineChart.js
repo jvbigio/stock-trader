f193c0fbe462ddacbacf5c121c3e67e36d4a93f2
Joe Bigio <jvbigio@gmail.com>
2021-10-27 19:49:27 -0400

    Working on formatting line chart

--- client/src/components/LineChart.js
+++ client/src/components/LineChart.js
@@ -4,28 +4,41 @@ import { Chart } from 'react-google-charts'
 
 const LineChart = () => {
   return (
-    <div>
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
       <Chart
         // width='600px'
-        width={800}
+        width={900}
         // height='400px'
         height={300}
         chartType='LineChart'
         loader={<div>Loading Chart</div>}
+        // data={[
+        //   ['x', 'Current Balance'],
+        //   [0, 0],
+        //   [1, 10],
+        //   [2, 23],
+        //   [3, 17],
+        //   [4, 18],
+        //   [5, 9],
+        //   [6, 11],
+        //   [7, 27],
+        //   [8, 33],
+        //   [9, 40],
+        //   [10, 32],
+        //   [11, 35]
+        // ]}
         data={[
-          ['x', 'Current Balance'],
-          [0, 0],
-          [1, 10],
-          [2, 23],
-          [3, 17],
-          [4, 18],
-          [5, 9],
-          [6, 11],
-          [7, 27],
-          [8, 33],
-          [9, 40],
-          [10, 32],
-          [11, 35]
+          [
+            { type: 'date', label: 'Day' },
+            'Current Balance'
+          ],
+          // [new Date(2021, 9), 5000],
+          // [new Date(2021, 9), 6400]
+          [new Date('2021-10-22'), 5000],
+          [new Date('2021-10-23'), 6200],
+          [new Date('2021-10-24'), 5900],
+          [new Date('2021-10-25'), 6000],
+          [new Date('2021-10-26'), 7000]
         ]}
         options={{
           hAxis: {
