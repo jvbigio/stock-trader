49790782a92495994337563330881da48f32b221
Joe Bigio <jvbigio@gmail.com>
2021-10-30 15:53:44 -0400

    debugging table

--- client/src/components/SellModal.js
+++ client/src/components/SellModal.js
@@ -23,17 +23,17 @@ export default function BuyModal () {
 
   return (
     <>
-      {/* <Tooltip
+      <Tooltip
         title='Sell'
         placement='right'
         arrow
         TransitionComponent={Fade}
         TransitionProps={{ timeout: 800 }}
-      > */}
-      {/* orig icon way: */}
-      {/* <SellIcon onClick={handleOpen} sx={{ cursor: 'pointer' }} color='action' /> */}
-      {/* <SellIcon onClick={handleOpen} sx={{ cursor: 'pointer' }} color='action' /> */}
-      {/* </Tooltip> */}
+      >
+        {/* orig icon way: */}
+        <SellIcon onClick={handleOpen} sx={{ cursor: 'pointer' }} color='action' />
+
+      </Tooltip>
       <Modal
         open={open}
         onClose={handleClose}
