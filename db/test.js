3f393b2cdcd6569741e61ca6f6769941b058133b
Joe Bigio <jvbigio@gmail.com>
2021-12-11 12:19:16 -0500

    Changed db test from car to person table

--- db/test.js
+++ db/test.js
@@ -2,7 +2,7 @@ const pool = require('./index')
 
 const testDB = async () => {
   try {
-    const response = await pool.query('SELECT * FROM car')
+    const response = await pool.query('SELECT * FROM person')
     console.log(response)
   } catch (err) {
     console.error(err)
