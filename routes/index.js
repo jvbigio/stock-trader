bce78e308b1f8cfd0e1cdde2ad6d30684f2994fa
Joe Bigio <jvbigio@gmail.com>
2021-12-11 13:47:58 -0500

    imported pool to routes

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,10 @@
 const express = require('express')
 const axios = require('axios').default
-const tokenService = require('../controllers/token-service')
-// const dummyData = require('../DummyData')
+const pool = require('../db/index')
+
 require('dotenv').config()
 
 const router = express.Router()
-
-/*
-IEX sandbox:
-
-https://sandbox.iexapis.com/stable/search/tsla?token=<API_KEY>
-
-https://sandbox.iexapis.com/stable/stock/aapl/quote?token=<API_KEY>
-
-https://sandbox.iexapis.com/stable/ref-data/symbols?token=<API_KEY>
-
-*/
 
 router.get('/stocks/buy', async (req, res) => {
   const token = process.env.API_SANDBOX_KEY
