0ffce1ead497e5f9f298a00afd86ebd1f6992919
Joe Bigio <jvbigio@gmail.com>
2021-12-15 17:56:04 -0500

    added notes to routes/index for TODOs

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,10 @@ require('dotenv').config()
 
 const router = express.Router()
 
+// create new transaction in transaction table, then run SQL statement
+// ... INSERT INTO transaction <include all data> ... then return that data back
 router.get('/stocks/buy', async (req, res) => {
   const token = process.env.API_SANDBOX_KEY
-
   try {
     const response = await axios.get(
       `https://sandbox.iexapis.com/stable/stock/${req.query.stock_symbol}/quote?token=${token}`
