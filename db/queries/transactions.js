bfb4fd4d51e58290c0606912543e5efdc0236604
Joe Bigio <jvbigio@gmail.com>
2021-12-29 20:15:02 -0500

    Added table column comments

--- db/queries/transactions.js
+++ db/queries/transactions.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const pool = require('../../db/index')
 
+// table columns: id, name, symbol, price, value, quantity, created_at, user_id
 const buyStock = async (companyName, symbol, latestPrice) => {
   const buy = await pool.query(
     'INSERT INTO holdings(name, symbol, price) VALUES($1, $2, $3) RETURNING *',
