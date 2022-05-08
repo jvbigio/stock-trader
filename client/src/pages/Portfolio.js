67067c39cf48603279b341d2551cf46e98f29e8b
Joe Bigio <jvbigio@gmail.com>
2022-05-08 14:08:12 -0400

    working on ideas for userCashBalance. try to use just useState on each buy

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -30,15 +30,52 @@ const Portfolio = () => {
   // this is the function that will be called when the user clicks the buy button
   // create a new function that will handle users CashBalance? Separation of concerns?
   // but the option to buy a stock relies on having the cash available...
+  // const fetchData = async () => {
+  //   // original fetchData
+  //   const buyStockUrl = `/api/stocks/buy?stock_symbol=${inputs.stockSymbol}`
+  //   const data = {
+  //     symbol: inputs.stockSymbol,
+  //     // quantity: inputs.shareAmount
+  //     amount: inputs.shareAmount // original
+  //   }
+  //   const response = await axios.post(buyStockUrl, data)
+  //   setStockData(response.data)
+  // }
+
+  // refactored fetchData
   const fetchData = async () => {
     const buyStockUrl = `/api/stocks/buy?stock_symbol=${inputs.stockSymbol}`
     const data = {
       symbol: inputs.stockSymbol,
-      // quantity: inputs.shareAmount
-      amount: inputs.shareAmount // original
+      amount: inputs.shareAmount
     }
-    const response = await axios.post(buyStockUrl, data)
-    setStockData(response.data)
+    // iterate over userTable to get the holding value of the stock
+    // if userCashBalance is greater than the holding value, then buy the stock
+    // then subtract the holding value from the user's cash balance
+    // then add the stock to the user's table
+    // else, alert the user that they don't have enough cash
+    const userTable = await axios.get('/api/stocks/user')
+    // try creating state to keep track of user's cash balance
+    // const userCashBalance = await axios.get('/api/stocks/user')
+    // console.log(userCashBalance)
+    // console.log(userTable)
+    // console.log(userCashBalance)
+    // console.log(userTable.data[0].cash_balance)
+    // console.log(userTable.data[0].holdings)
+    // console.log(userTable.data[0].holdings[0].value)
+    // console.log(userTable.data[0].holdings[0].symbol)
+    // console.log(userTable.data[0].holdings[0].quantity)
+    // console.log(userTable.data[0].holdings[0].amount)
+    // console.log(userTable.data[0].holdings[0].total_value)
+    userTable.data.forEach(stock => { // causes too many requests
+      if (userCashBalance > stock.value) {
+        axios.post(buyStockUrl, data)
+        setUserCashBalance(prevState => prevState - stock.value)
+        setUserTable(prevState => [...prevState, stock])
+      } else {
+        alert('You do not have enough cash to buy this stock')
+      }
+    })
   }
 
   // // testing
@@ -55,14 +92,17 @@ const Portfolio = () => {
   //   const updateCashBalance = () => {
   //     userTable.forEach(stock => {
   //       // console.log(stock.value)
-  //       setUserCashBalance(prevState => prevState - stock.value)
-  //       // console.log(userCashBalance)
-  //       console.log(stock.value)
+  //       if (userCashBalance > stock.value) {
+  //         setUserCashBalance(prevState => prevState - stock.value)
+  //         console.log(userCashBalance)
+  //         // console.log(stock.value)
+  //       } else if (userCashBalance < stock.value) {
+  //         console.log('not enough cash')
+  //       }
   //     })
-  //     // console.log(userCashBalance)
   //   }
   //   updateCashBalance()
-  // }, [userTable])
+  // })
 
   // option 2
   // useEffect(() => {
@@ -83,24 +123,24 @@ const Portfolio = () => {
 
   // option 3
   // maybe instead of useEffect try to use /buy route?
-  useEffect(() => {
-    const handleUserCashBalance = () => {
-      userTable.forEach(stock => {
-        // console.log(stock.value)
-        if (userCashBalance > 0 && userCashBalance > stock.value) {
-          setUserCashBalance(prevState => prevState - stock.value)
-          // console.log(userCashBalance)
-        } else if (userCashBalance <= 0) {
-          console.log('no more money')
-          // stop function
-        }
-      })
-      // console.log(userCashBalance)
-    }
-    handleUserCashBalance()
-  }, [userCashBalance, userTable])
+  // useEffect(() => {
+  //   const handleUserCashBalance = () => {
+  //     userTable.forEach(stock => {
+  //       // console.log(stock.value)
+  //       if (userCashBalance > 0 && userCashBalance > stock.value) {
+  //         setUserCashBalance(prevState => prevState - stock.value)
+  //         // console.log(userCashBalance)
+  //       } else if (userCashBalance <= 0) {
+  //         console.log('no more money')
+  //         // stop function
+  //       }
+  //     })
+  //     // console.log(userCashBalance)
+  //   }
+  //   handleUserCashBalance()
+  // }, [userCashBalance, userTable])
 
-  // console.log(userTable) // works. work on iterating through the user's stocks
+  console.log(userTable) // works. work on iterating through the user's stocks
 
   useEffect(() => {
     getUserHoldings()
